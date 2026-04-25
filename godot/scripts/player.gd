1c7bc68d91fd9fe5453cecee168da54d428d5b4e
olek18-blip <olek18@hotmail.com>
2026-04-25 22:56:27 +0200

    Actualizar player.gd

--- godot/scripts/player.gd
+++ godot/scripts/player.gd
@@ -3,7 +3,12 @@ extends CharacterBody3D
 @export var speed := 6.0
 var gravity := ProjectSettings.get_setting("physics/3d/default_gravity")
 
+var current_car = null
+
 func _physics_process(delta):
+    if current_car:
+        return  # NO controlar player si está en coche
+
     if not is_on_floor():
         velocity.y -= gravity * delta
 
@@ -18,3 +23,29 @@ func _physics_process(delta):
         velocity.z = move_toward(velocity.z, 0, speed)
 
     move_and_slide()
+
+func _input(event):
+    if event.is_action_pressed("interact"):
+        if current_car:
+            exit_car()
+        else:
+            try_enter_car()
+
+func try_enter_car():
+    var cars = get_tree().get_nodes_in_group("car")
+
+    for car in cars:
+        if global_position.distance_to(car.global_position) < 3:
+            current_car = car
+            car.enter_car(self)
+            visible = false
+            return
+
+func exit_car():
+    if not current_car:
+        return
+
+    global_position = current_car.global_position + Vector3(2,0,0)
+    current_car.exit_car()
+    current_car = null
+    visible = true
